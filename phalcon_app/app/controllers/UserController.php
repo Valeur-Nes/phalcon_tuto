8d3281fe2c9c7714a8b8ec26766c0e767a45613c
Fanilo Raveloson <raveloson.fanilo@gmail.com>
2021-02-08 17:30:15 +0100

    .

--- phalcon_app/app/controllers/UserController.php
+++ phalcon_app/app/controllers/UserController.php
@@ -1,207 +1,210 @@
 <?php
+
 declare(strict_types=1);
 
 
 class UserController extends \Phalcon\Mvc\Controller
 {
 
-	/**
-	 * Show all Users
-	 */
+    /**
+     * Show all Users
+     */
     public function indexAction()
     {
-		$this->view->setVar('users', User::find());
+        $this->view->setVar('users', User::find());
     }
 
-	/**
-	 * @param int $id
-	 * Show one User
-	 */
-    public function showAction(int $id) {
-    	$this->view->setVar('user', User::findFirst($id));
+    /**
+     * @param int $id
+     * Show one User
+     */
+    public function showAction(int $id)
+    {
+        $this->view->setVar('user', User::findFirst($id));
     }
 
-	/**
-	 * Display create form
-	 */
-    public function newAction() {
-    	// Display creation user
+    /**
+     * Display create form
+     */
+    public function newAction()
+    {
+        // Display creation user
     }
 
-	/**
-	 * Save new user in database
-	 */
-    public function createAction() {
+    /**
+     * Save new user in database
+     */
+    public function createAction()
+    {
 
-	    if (!$this->request->isPost() or empty($this->request)) {
-		    $this->dispatcher->forward([
-			    'controller' => "User",
-			    'action' => 'index'
-		    ]);
-		    return;
-	    }
+        if (!$this->request->isPost() or empty($this->request)) {
+            $this->dispatcher->forward([
+                'controller' => "User",
+                'action' => 'index'
+            ]);
+            return;
+        }
 
-	    $user = (new User())
-		    ->setLastname($this->request->getPost('lastname', 'string'))
-		    ->setFirstname($this->request->getPost('firstname', 'string'))
-		    ->setEmail($this->request->getPost('email', 'string'))
-		    ->setPassword($this->security->hash($this->request->getPost('password', 'string')))
-	    ;
+        $user = (new User())
+            ->setLastname($this->request->getPost('lastname', 'string'))
+            ->setFirstname($this->request->getPost('firstname', 'string'))
+            ->setEmail($this->request->getPost('email', 'string'))
+            ->setPassword($this->security->hash($this->request->getPost('password', 'string')));
 
-	    $userFound = User::findFirst([
-		    "(email = :email:)",
-		    'bind' => [
-			    'email' => $user->getEmail()
-		    ]
-	    ]);
+        $userFound = User::findFirst([
+            "(email = :email:)",
+            'bind' => [
+                'email' => $user->getEmail()
+            ]
+        ]);
 
-	    if ($userFound == null) {
-		    if (!$user->save()) {
-			    foreach ($user->getMessages as $message) {
-				    $this->flash->error($message);
-			    }
-		    }
+        if ($userFound == null) {
+            if (!$user->save()) {
+                foreach ($user->getMessages as $message) {
+                    $this->flash->error($message);
+                }
+            }
 
-		    $this->dispatcher->forward([
-			    'controller' => 'User',
-			    'action' => 'index'
-		    ]);
-		    return;
-	    }
+            $this->dispatcher->forward([
+                'controller' => 'User',
+                'action' => 'index'
+            ]);
+            return;
+        }
 
-	    $this->dispatcher->forward([
-		    'controller' => 'User',
-		    'action' => 'index'
-	    ]);
-
+        $this->dispatcher->forward([
+            'controller' => 'User',
+            'action' => 'index'
+        ]);
     }
 
-    public function editAction($id) {
-	    if (!$this->request->isPost()) {
-		    $user = User::findFirst($id);
-		    if (!$user) {
-			    $this->flash->error("user was not found");
+    public function editAction($id)
+    {
+        if (!$this->request->isPost()) {
+            $user = User::findFirst($id);
+            if (!$user) {
+                $this->flash->error("user was not found");
 
-			    $this->dispatcher->forward([
-				    'controller' => "user",
-				    'action' => 'index'
-			    ]);
+                $this->dispatcher->forward([
+                    'controller' => "user",
+                    'action' => 'index'
+                ]);
 
-			    return;
-		    }
+                return;
+            }
 
-		    $this->view->id = $user->getId();
-		    $this->tag->setDefault("lastname", $user->getLastname());
-		    $this->tag->setDefault("firstname", $user->getFirstname());
-		    $this->tag->setDefault("email", $user->getEmail());
-		    $this->tag->setDefault("password", $user->getPassword());
-	    }
-
+            $this->view->id = $user->getId();
+            $this->tag->setDefault("lastname", $user->getLastname());
+            $this->tag->setDefault("firstname", $user->getFirstname());
+            $this->tag->setDefault("email", $user->getEmail());
+            $this->tag->setDefault("password", $user->getPassword());
+        }
     }
 
-    public function saveAction() {
+    public function saveAction()
+    {
 
-	    if (!$this->request->isPost()) {
-		    $this->dispatcher->forward([
-			    'controller' => "user",
-			    'action' => 'index'
-		    ]);
+        if (!$this->request->isPost()) {
+            $this->dispatcher->forward([
+                'controller' => "user",
+                'action' => 'index'
+            ]);
 
-		    return;
-	    }
+            return;
+        }
 
-	    $id = $this->request->getPost("id");
-	    $user = User::findFirst($id);
+        $id = $this->request->getPost("id");
+        $user = User::findFirst($id);
 
-	    if (!$user) {
-		    $this->flash->error("user does not exist " . $id);
+        if (!$user) {
+            $this->flash->error("user does not exist " . $id);
 
-		    $this->dispatcher->forward([
-			    'controller' => "user",
-			    'action' => 'index'
-		    ]);
+            $this->dispatcher->forward([
+                'controller' => "user",
+                'action' => 'index'
+            ]);
 
-		    return;
-	    }
+            return;
+        }
 
-	    $user->setLastname($this->request->getPost("lastname", "string"));
-	    $user->setFirstname($this->request->getPost("firstname", "string"));
-	    $user->setEmail($this->request->getPost("email", "string"));
-	    $user->setPassword($this->request->getPost("password", "string"));
+        $user->setLastname($this->request->getPost("lastname", "string"));
+        $user->setFirstname($this->request->getPost("firstname", "string"));
+        $user->setEmail($this->request->getPost("email", "string"));
+        $user->setPassword($this->request->getPost("password", "string"));
 
-	    if (!$user->save()) {
+        if (!$user->save()) {
 
-		    foreach ($user->getMessages() as $message) {
-			    $this->flash->error($message);
-		    }
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
 
-		    $this->dispatcher->forward([
-			    'controller' => "user",
-			    'action' => 'edit',
-			    'params' => [$user->getId()]
-		    ]);
+            $this->dispatcher->forward([
+                'controller' => "user",
+                'action' => 'edit',
+                'params' => [$user->getId()]
+            ]);
 
-		    return;
-	    }
+            return;
+        }
 
-	    $this->flash->success("user was updated successfully");
+        $this->flash->success("user was updated successfully");
 
-	    $this->dispatcher->forward([
-		    'controller' => "user",
-		    'action' => 'index'
-	    ]);
-
+        $this->dispatcher->forward([
+            'controller' => "user",
+            'action' => 'index'
+        ]);
     }
 
-    public function deleteAction($id) {
-	    $user = User::findFirst($id);
+    public function deleteAction($id)
+    {
+        $user = User::findFirst($id);
 
-	    if (!$user) {
-		    $this->flash->error("user was not found");
+        if (!$user) {
+            $this->flash->error("user was not found");
 
-		    $this->dispatcher->forward([
-			    'controller' => "user",
-			    'action' => 'index'
-		    ]);
+            $this->dispatcher->forward([
+                'controller' => "user",
+                'action' => 'index'
+            ]);
 
-		    return;
-	    }
+            return;
+        }
 
-	    if (!$user->delete()) {
+        if (!$user->delete()) {
 
-		    foreach ($user->getMessages() as $message) {
-			    $this->flash->error($message);
-		    }
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
 
-		    $this->dispatcher->forward([
-			    'controller' => "user",
-			    'action' => 'search'
-		    ]);
+            $this->dispatcher->forward([
+                'controller' => "user",
+                'action' => 'search'
+            ]);
 
-		    return;
-	    }
+            return;
+        }
 
-	    $this->flash->success("user was deleted successfully");
+        $this->flash->success("user was deleted successfully");
 
-	    $this->dispatcher->forward([
-		    'controller' => "user",
-		    'action' => "index"
-	    ]);
-
+        $this->dispatcher->forward([
+            'controller' => "user",
+            'action' => "index"
+        ]);
     }
 
-    public function loginAction() {
-	    if ($this->request->isPost()) {
-		    $email = $this->request->getPost('email');
-		    $password = $this->request->getPost('password');
-		    $user = User::findFirst(["(email = :email:) AND password = :password:", 'bind' => ['email' => $email, 'password' => $this->security->hash($password)]]);
-        
+    public function loginAction()
+    {
+        if ($this->request->isPost()) {
+            $email = $this->request->getPost('email');
+            $password = $this->request->getPost('password');
+            $user = User::findFirst(["(email = :email:) AND password = :password:", 'bind' => ['email' => $email, 'password' => $this->security->hash($password)]]);
+
             if ($user !== false) {
                 $this->_registerSession($user);
                 $this->flash->success(
                     'Welcome ' . $user->name
                 );
-                // rediriger vers la page d'accueil après connexion réussie
+                
                 return $this->dispatcher->forward(
                     [
                         'controller' => 'user',
@@ -212,17 +215,18 @@ class UserController extends \Phalcon\Mvc\Controller
         }
     }
 
-    public function boardAction() {
-
+    public function boardAction()
+    {
+        $this->view->firstname = $this->session->get('firstname');
+        $this->view->lastname = $this->session->get('lastname');
     }
 
-    public function _registerSession($user) {
-    	$this->session->set('auth', [
-    		'id' => $user->id,
-		    'firstname' => $user->firstname,
-		    'lastname' => $user->lastname
-	    ]);
+    public function _registerSession($user)
+    {
+        $this->session->set('auth', [
+            'id' => $user->id,
+            'firstname' => $user->firstname,
+            'lastname' => $user->lastname
+        ]);
     }
-
 }
-
